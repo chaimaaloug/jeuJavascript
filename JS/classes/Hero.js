d20a7382b3bb723a3df34aecd655f3d16b131407
bore-prog <philippeaurore@orange.fr>
2022-11-07 23:13:59 +0100

    feat(Magasin, Argent, Soin)+Refacto
    Ajout du magasin, de l'argent et des potions de soin.
    Modif d'ID pour clarté du code
    Ajout setter pour la défense (pour prendre en compte la défense des monstres)

--- JS/classes/Hero.js
+++ JS/classes/Hero.js
@@ -2,13 +2,21 @@
 
 // export 
 class Hero extends Personnage {
-    constructor(nom, vie, attaque, defense, experience, niveau){
+    constructor(nom, vie, attaque, defense, experience, niveau, argent, potionSoin){
         super(nom, vie, attaque, defense, niveau);
 
         this.experience = experience;
+        this.argent = argent;
+        this.potionSoin = potionSoin;
     }
 
     getExperience() {
         return this.experience;
+    }
+    getArgent(){
+        return this.argent;
+    }
+    getPotionSoin(){
+        return this.potionSoin;
     }
 }
--- JS/classes/Monstre.js
+++ JS/classes/Monstre.js
@@ -2,14 +2,18 @@
 
 // export 
 class Monstre extends Personnage {
-    constructor(nom, vie, attaque, defense, experienceDonnee, img){
+    constructor(nom, vie, attaque, defense, experienceDonnee, img, argentDonne){
         super(nom, vie, attaque, defense);
 
         this.experienceDonnee = experienceDonnee;
         this.img = img;
+        this.argentDonne = argentDonne
     }
 
     getExperienceDonnee() {
         return this.experienceDonnee;
+    }
+    getArgentDonne(){
+        return this.argentDonne;
     }
 }
